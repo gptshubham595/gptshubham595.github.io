24823e13667ab48a27ecced17cec619a7f50515b
gptshubham595 <gptshubham595@gmail.com>
2020-11-07 02:57:07 +0530

    done

---
+++ Android/includes/Constants.php
@@ -0,0 +1,12 @@
+<?php 
+	define('DB_NAME','shubh40_android');
+	define('DB_USER','shubh40');
+	define('DB_PASSWORD','r54Op{7Dzjxe');
+	define('DB_HOST','localhost');
+	
+	$username = "shubh40"; 
+    $password = "r54Op{7Dzjxe"; 
+    $host = "localhost"; 
+    $dbname = "shubh40_android_login"; 
+?>
+	
